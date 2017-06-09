290d0a7eb42be30dfecf2c69814f0eee5e2fa07d
John <jaspajjr@gmail.com>
2017-06-10 00:08:48 +0100

    Added pika logic to webLogger

--- webLogger/app.py
+++ webLogger/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import uuid
 import datetime
 import time
+import pika
 app = Flask(__name__)
 
 
@@ -15,8 +16,28 @@ def logger():
     session_information = [session_id, start_time, day_of_week, referrer,
                            user_agent]
     d = {'info': session_information}
+    send_to_MQ(d)
     return jsonify(d)
 
 
 if __name__ == '__main__':
     app.run()
+
+
+def send_to_MQ(message):
+    '''
+    Sends an acknowledgement message to the exchange that pygbq has finished
+    running.
+    '''
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='RABBIT'))
+    channel = connection.channel()
+    channel.exchange_declare(
+        exchange='0.0.0.0:15672',
+        exchange_type='fanout')
+    channel.basic_publish(exchange='PYGBQ_EXCHANGE',
+                          routing_key='',
+                          body=message)
+
+    print " [x] Sent message"
+    connection.close()
