1ca53614f5567f23cdb43711524aad33d2ce9130
John <jaspajjr@gmail.com>
2017-06-10 00:46:27 +0100

    Haven't ssorted out conneting to rabbitmq yet, but we have an instance up and running. We also have a basic template set up in flask

--- webLogger/app.py
+++ webLogger/app.py
@@ -1,9 +1,28 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import uuid
 import datetime
 import time
 import pika
 app = Flask(__name__)
+
+
+def send_to_MQ(message):
+    '''
+    Sends an acknowledgement message to the exchange that pygbq has finished
+    running.
+    '''
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='0.0.0.0:15672'))
+    channel = connection.channel()
+    channel.exchange_declare(
+        exchange='EXAMPLE',
+        exchange_type='fanout')
+    channel.basic_publish(exchange='PYGBQ_EXCHANGE',
+                          routing_key='',
+                          body=message)
+
+    print " [x] Sent message"
+    connection.close()
 
 
 @app.route('/')
@@ -16,28 +35,10 @@ def logger():
     session_information = [session_id, start_time, day_of_week, referrer,
                            user_agent]
     d = {'info': session_information}
+    return render_template('layout.html')
     send_to_MQ(d)
     return jsonify(d)
 
 
 if __name__ == '__main__':
     app.run()
-
-
-def send_to_MQ(message):
-    '''
-    Sends an acknowledgement message to the exchange that pygbq has finished
-    running.
-    '''
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='RABBIT'))
-    channel = connection.channel()
-    channel.exchange_declare(
-        exchange='0.0.0.0:15672',
-        exchange_type='fanout')
-    channel.basic_publish(exchange='PYGBQ_EXCHANGE',
-                          routing_key='',
-                          body=message)
-
-    print " [x] Sent message"
-    connection.close()
