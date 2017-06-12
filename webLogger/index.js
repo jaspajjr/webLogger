b308c1dde12d43d7fc82f254ba6d0244123557a1
Duckzbug <domduxbury@hotmail.com>
2017-06-12 17:37:31 +0100

    Added cookie parser

--- webLogger/index.js
+++ webLogger/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
 
 app.get('/', function (req, res) {
-  var file_path = "/templates/layout.html"
-  var fullpath = __dirname + file_path;
-  res.sendFile(fullpath)
+  let file_path = "/templates/layout.html"
+  let fullpath = __dirname + file_path;
+
+  res.cookie(cookie_name , 'cookie_value').sendFile(fullpath)
+  console.log(req.headers)
 })
 
-app.listen(3000, function () {
+app.listen(3000, function (req, res) {
   console.log('Example app listening on port 3000!')
 })
